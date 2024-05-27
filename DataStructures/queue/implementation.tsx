3344de1aba3edfd9d2c044a0939aabcbdc6cd9a7
Vihanga Rashan Silva <vihangarashansilva@gmail.com>
2024-05-27 23:53:25 +0530

    Add: Queue Implementation

---
+++ DataStructures/queue/implementation.tsx
@@ -0,0 +1,34 @@
+
+export class Queue<T> {
+  _queue: T[];
+
+  constructor(queue?: T[]) {
+    this._queue = queue || [];
+  }
+
+  enqueue(item: T) {
+    this._queue.push(item);
+  }
+
+  dequeue(): T | undefined {
+    return this._queue.shift()
+  }
+
+  clear() {
+    this._queue = [];
+  }
+
+  get front(): T {
+    return this._queue[0]
+  }
+
+  get isEmpty(): boolean {
+    if (this._queue.length > 0) {
+      return false
+    } return true;
+  }
+
+  get count(): number {
+    return this._queue.length;
+  }
+}
